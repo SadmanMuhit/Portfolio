254230847d03b41651838d3b34698dc299066195
SadmanMuhit <sadmanmuhit@gmail.com>
2025-04-23 23:44:41 +0600

    all edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { Route, Router, Routes } from "react-router-dom";
 import Home from "./Page/Home";
-import Layout from "./Components/Layout/Layout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-      </Route>
-    </Routes>
+        <Home/>
   );
 }
 
--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
       <div className='container m-auto flex justify-between items-center p-[26px]'>
-        <Link to="/" smooth={true} duration={500} className='text-blue-50 font-poppins text-2xl font-bold'>SSMuhit</Link>
+        <a to="#" className='text-blue-50 font-poppins text-2xl font-bold'>SSMuhit</a>
         <ul className='flex gap-8 font-poppins font-normal text-[14px]'> 
-          <li><Link to="/" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="/" smooth={true} duration={500}>About</Link></li>
-          <li><Link to="/" smooth={true} duration={500}>Projects</Link></li>
-          <li><Link to="/" smooth={true} duration={500}>Skills</Link></li>
-          <li><Link to="/" smooth={true} duration={500}>Contact</Link></li>
+          <li><a to="#">Home</a></li>
+          <li><a to="#">About</a></li>
+          <li><a to="#">Projects</a></li>
+          <li><a to="#">Skills</a></li>
+          <li><a to="#">Contact</a></li>
         </ul>
         <button className='bg-bg pt-2 pb-2 pr-5 pl-5 rounded-full text-white text-[16px]'>Hire Me</button>
       </div>
