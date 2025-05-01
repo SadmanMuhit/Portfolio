fdf0b51bb32bf1f6e803bb63b3b7f5557fa9949a
SadmanMuhit <sadmanmuhit@gmail.com>
2025-05-01 10:01:29 +0600

    size

--- src/Components/Home/Singlepage.jsx
+++ src/Components/Home/Singlepage.jsx
@@ -59,16 +59,16 @@ const Singlepage = () => {
       {/* Navbar end */}
 
       {/* Banner start */}
-      <section className="bg-banbg md:p-[60px]" id="Banner">
+      <section className="bg-banbg p-[60px] sm:px-[60px] px-4" id="Banner">
         <div className="container m-auto text-left">
           <div className=" justify-between items-center">
             <img
               src="/banner.jpg"
               alt="banner-image"
-              className="float-right rounded-2xl bg-imagebg p-3 w-[400px] h-[400px]"
+              className="float-right rounded-2xl bg-imagebg p-3  h-[400px] w-[400px]"
             />
           </div>
-          <div className="w-full text-center"> 
+          <div className="w-full"> 
           <h1 className="font-poppins text-4xl">
             Hi,i'm Muhit
             <span className="block text-text font-bold text-6xl">
@@ -77,11 +77,11 @@ const Singlepage = () => {
             <span className="text-text font-bold text-6xl">Developer</span>
           </h1>
           </div>
-          <p className="font-normal text-[20px] text-p mt-6 md:mt-10 text-center">
+          <p className="font-normal text-[20px] text-p mt-6 md:mt-10">
             I create beautiful and functional websites that help <br />
             businesses grow and succeed online.
           </p>
-          <div className="flex gap-4 mt-10 text-center items-center justify-center">
+          <div className="flex gap-4 mt-10">
             <button className="hover:bg-bg text-[16px] border border-border font-normal py-2 px-6 rounded-full hover:text-white shadow duration-300">
               Contact Me
             </button>
@@ -89,7 +89,7 @@ const Singlepage = () => {
               View Work
             </button>
           </div>
-          <div className="mt-8 flex gap-6 text-2xl justify-center pb-2">
+          <div className="mt-8 flex gap-6 text-2xl ">
             <Link target="_blank" to="https://github.com/SadmanMuhit">
               <FaGithub />
             </Link>
@@ -108,7 +108,7 @@ const Singlepage = () => {
       {/* Banner end */}
 
       {/* About start */}
-      <section id="About" className="md:px-[60px]">
+      <section id="About" className="md:px-[60px] sm:px-[60px] px-4">
         <div className="container m-auto">
           <div>
           <h4 className="font-poppins text-3xl font-bold text-center mt-20">
@@ -121,7 +121,7 @@ const Singlepage = () => {
               className="mt-[60px] float-left mr-12 rounded-2xl w-[400px] h-[400px]"
             />
           </div>
-          <div className="mt-[109px] ml-5">
+          <div className="mt-[109px] ml-5 ">
             <p className="text-2xl font-poppins font-bold text-blue-50">
               A passionate developer with 1+ years of <br /> experience
             </p>
@@ -140,7 +140,7 @@ const Singlepage = () => {
       {/* About end */}
 
       {/* Project start */}
-      <section className="bg-banbg" id="Project">
+      <section className="bg-banbg sm:px-[60px] px-4" id="Project">
         <div className="container m-auto">
           <div>
             <h2 className="font-poppins text-3xl font-bold text-center pt-[75px]">
@@ -148,20 +148,20 @@ const Singlepage = () => {
             </h2>
           </div>
             <div className="gap-8 mt-[60px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-20">
-              <div className="image4">
+              <div className="image4 ">
                 <img
                   src="/project.png"
                   alt=""
-                  className="w-[395px] rounded-tr-[60px] rounded-tl-[60px]"
+                  className=" rounded-tr-[60px] rounded-tl-[60px]"
                 />
-                <div className="bg-white w-[395px] p-6 rounded-b-2xl rounded-bl-2xl">
+                <div className="bg-white p-6 rounded-b-2xl rounded-bl-2xl ">
                   <h6 className="text-[20px] font-bold font-poppins">
                     kraft project
                   </h6>
                   <p className="text-base font-normal font-poppins mt-3">
                     A creative studio Product website and home page
                   </p>
-                  <div className="flex text-sm text-text mt-7 gap-2 mb-4">
+                  <div className="flex text-[12px] text-text mt-7 gap-2 mb-4">
                     <p className="bg-textbg py-1 px-3 rounded-full">
                       HTML & CSS
                     </p>
@@ -187,16 +187,16 @@ const Singlepage = () => {
                 <img
                   src="/project.png"
                   alt=""
-                  className="w-[395px] rounded-tr-[60px] rounded-tl-[60px]"
+                  className="rounded-tr-[60px] rounded-tl-[60px]"
                 />
-                <div className="bg-white w-[395px] p-6 rounded-b-2xl rounded-bl-2xl">
+                <div className="bg-white p-6 rounded-b-2xl rounded-bl-2xl">
                   <h6 className="text-[20px] font-bold font-poppins">
                     kraft project
                   </h6>
                   <p className="text-base font-normal font-poppins mt-3">
                     A creative studio Product website and home page
                   </p>
-                  <div className="flex text-sm text-text mt-7 gap-2 mb-4">
+                  <div className="flex text-[12px] text-text mt-7 gap-2 mb-4">
                     <p className="bg-textbg py-1 px-3 rounded-full">
                       HTML & CSS
                     </p>
@@ -222,16 +222,16 @@ const Singlepage = () => {
                 <img
                   src="/project.png"
                   alt=""
-                  className="w-[395px] rounded-tr-[60px] rounded-tl-[60px]"
+                  className=" rounded-tr-[60px] rounded-tl-[60px]"
                 />
-                <div className="bg-white w-[395px] p-6 rounded-b-2xl rounded-bl-2xl">
+                <div className="bg-white p-6 rounded-b-2xl rounded-bl-2xl">
                   <h6 className="text-[20px] font-bold font-poppins">
                     kraft project
                   </h6>
                   <p className="text-base font-normal font-poppins mt-3">
                     A creative studio Product website and home page
                   </p>
-                  <div className="flex text-sm text-text mt-7 gap-2 mb-4">
+                  <div className="flex text-[12px] text-text mt-7 gap-2 mb-4">
                     <p className="bg-textbg py-1 px-3 rounded-full">
                       HTML & CSS
                     </p>
@@ -252,20 +252,21 @@ const Singlepage = () => {
                     </Link>
                   </div>
                 </div>
-              </div>  <div className="image4">
+              </div>  
+              <div className="image4 pb-4">
                 <img
                   src="/project.png"
                   alt=""
-                  className="w-[395px] rounded-tr-[60px] rounded-tl-[60px]"
+                  className=" rounded-tr-[60px] rounded-tl-[60px]"
                 />
-                <div className="bg-white w-[395px] p-6 rounded-b-2xl rounded-bl-2xl">
+                <div className="bg-white p-6 rounded-b-2xl rounded-bl-2xl">
                   <h6 className="text-[20px] font-bold font-poppins">
                     kraft project
                   </h6>
                   <p className="text-base font-normal font-poppins mt-3">
                     A creative studio Product website and home page
                   </p>
-                  <div className="flex text-sm text-text mt-7 gap-2 mb-4">
+                  <div className="flex text-[12px] text-text mt-7 gap-2 mb-4">
                     <p className="bg-textbg py-1 px-3 rounded-full">
                       HTML & CSS
                     </p>
@@ -293,7 +294,7 @@ const Singlepage = () => {
       {/* Project end */}
 
       {/* Skill start */}
-      <section id="Skill">
+      <section id="Skill" className="sm:px-[60px] px-4"> 
         <div className="container m-auto mb-20">
           <h3 className="text-3xl font-bold font-poppins text-center mt-[75px] mb-[60px]">
             Skills & Expertise
